d930a0239a1debe1fd6547e262fc3a437e37771e
aryaIOS <arya.vashisht@eka.care>
2025-01-29 00:22:21 +0530

    Records communicator changes

--- Sources/Views/Grid/RecordsGridListViews.swift
+++ Sources/Views/Grid/RecordsGridListViews.swift
@@ -127,7 +127,7 @@ extension RecordsGridListView {
   }
   
   private func onDoneButtonPressed() {
-    RecordsCommunicator.shared.setPickerSelectedImagesFromRecords(selectedRecords: pickerSelectedRecords)
+    RecordsCommunicator().setPickerSelectedImagesFromRecords(selectedRecords: pickerSelectedRecords)
   }
 }
 
