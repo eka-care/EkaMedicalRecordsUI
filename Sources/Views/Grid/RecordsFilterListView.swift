c8cd912d642ac85a3cfe1be38515061c296eea08
aryaIOS <arya.vashisht@eka.care>
2025-04-10 12:07:56 +0530

    Added upload sync

--- Sources/Views/Grid/RecordsFilterListView.swift
+++ Sources/Views/Grid/RecordsFilterListView.swift
@@ -10,7 +10,7 @@ import EkaMedicalRecordsCore
 
 struct RecordsFilterListView: View {
   
-//  @State private var selectedChip: RecordDocumentType = .
+//  @State private var selectedChip: RecordDocumentType = 
   
   var body: some View {
     Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
