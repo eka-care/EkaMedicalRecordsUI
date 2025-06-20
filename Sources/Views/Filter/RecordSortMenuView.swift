dd1dccc15a4a124bd431f9018e18b39b87ffd060
aryaIOS <arya.vashisht@eka.care>
2025-06-20 14:59:10 +0530

    Added default checkmark

--- Sources/Views/Filter/RecordSortMenuView.swift
+++ Sources/Views/Filter/RecordSortMenuView.swift
@@ -19,11 +19,13 @@ struct RecordSortMenuView: View {
           HStack {
             Text(option.title)
               .textStyle(ekaFont: .bodyRegular, color: .black)
+            /// If no option is selected show checkmark on upload date new to old
+            if selectedOption == nil, option == .dateOfUpload(sortingOrder: .newToOld) {
+              CheckMarkView()
+            }
+            /// If option is selected show that
             if option == selectedOption {
-              Image(systemName: "checkmark")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 12, height: 12, alignment: .center)
+              CheckMarkView()
             }
           }
         }
@@ -41,6 +43,17 @@ struct RecordSortMenuView: View {
         isSelected: selectedOption != nil
       ) {_ in}
     }
+  }
+}
+
+// MARK: - Subview
+
+extension RecordSortMenuView {
+  private func CheckMarkView() -> some View {
+    Image(systemName: "checkmark")
+      .resizable()
+      .scaledToFit()
+      .frame(width: 12, height: 12, alignment: .center)
   }
 }
 
