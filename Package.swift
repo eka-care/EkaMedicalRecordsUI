06f6ec71c5da8485ec3634cfb64cf6d60caf2d0c
ShekharEkaCare <shekhar.gupta@eka.care>
2025-09-03 12:31:50 +0530

    updated version

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/SnapKit/SnapKit.git", exact: Version(stringLiteral: "5.0.1")),
     .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0"),
-    .package(url: "https://github.com/eka-care/EkaMedicalRecordsCore.git", branch: "core_records_case_linking"),
-    .package(url: "https://github.com/eka-care/EkaUI.git", branch: "main")
+    .package(url: "https://github.com/eka-care/EkaMedicalRecordsCore.git", from: "1.1.3"),
+    .package(url: "https://github.com/eka-care/EkaUI.git", from: "1.1.0")
   ],
   targets: [
     .target(
