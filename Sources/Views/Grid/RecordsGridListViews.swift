34f339e771a6c84a7a05837d59dc895552e3b1ba
shekhar <shekhar.gupta@eka.care>
2026-03-02 15:20:35 +0530

    remove redundent code

--- Sources/Views/Grid/RecordsGridListViews.swift
+++ Sources/Views/Grid/RecordsGridListViews.swift
@@ -313,7 +313,6 @@ extension RecordsGridListView {
   }
   /// Used to delete a grid item
   private func deleteItem(record: Record) {
-    let isRecordLinked = record.syncState == RecordSyncState.upload(success: true).stringValue
     recordsRepo.deleteRecord(record: record)
   }
   
